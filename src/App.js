b86e72b2baf0c1d742b6b1d42a9aa21d0f43ced7
’lv36900 <1984493143@qq.com>
2020-03-13 21:04:04 +0800

    安装完antd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React,{Component} from "react"
+import {Button} from "antd"
 export default class App extends Component{
   render(){
     return(
-      <div>App</div>
+      <div>
+        App
+        <br/>
+        <div>
+    <Button type="primary" block>
+      Primary
+    </Button>
+    <Button block>Default</Button>
+    <Button type="dashed" block>
+      Dashed
+    </Button>
+    <Button type="link" block>
+      Link
+    </Button>
+  </div>,
+      </div>
     )
   }
 }
