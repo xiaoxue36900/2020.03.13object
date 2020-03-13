566f77c257ab4f00f29933c97cc840a99a191e44
’lv36900 <1984493143@qq.com>
2020-03-13 22:41:07 +0800

    静态页面完成

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,10 +1,17 @@
 import React,{Component} from "react"
-
+import logo from "../login/imgs/logo.png"
+import "./css/login.less"
 export default class Login extends Component{
   render(){
     return(
-      <div>
-        <h1>Login</h1>
+      <div id="login">
+        <header className="login-header">
+          <img src={logo} alt=""/>
+          <h2>商品管理系统</h2>
+        </header>
+        <div className='login-content'>
+          <h3>用户登录</h3>
+        </div>
       </div>
     )
   }
